59f1f556ee0573c72150a02a9813215e7ebe42e1
cariholmes <holmes.l.cari@gmail.com>
2019-09-17 16:58:12 -0500

    end of day working on checkbook

--- checkbook.py
+++ checkbook.py
@@ -1,9 +1,10 @@
+# Welcome to my application!
 print("~~ Welcome to your checkbook! ~~\n")
 
-
+# How the user will be prompted
 prompt = '> Your option: '
 
-
+# menu options the user can pick from to proceed
 def menu():
     print("How would you like to proceed?")
     print("""
@@ -13,21 +14,24 @@ def menu():
     4) View savings  
     5) Exit\n""")
 
-
+# opens and reads the checkbook text file balance
 def read_balance():
     f = open("checkbook.txt", "r")
     bal = f.read()
     return bal
 
+# opens and writes to the checkbook text file
 def write_balance(new_balance):
     f = open("checkbook.txt", "w")
     f.write(str(new_balance))
 
+# this function takes the read_balance function as current_balance and adds the deposit amount to it to create a new balance, then calls the write_balance(new_balance) function to display the new amount
 def make_deposit(deposit_amount):
     current_amount = read_balance()
     new_balance = int(current_amount) + int(deposit_amount)
     write_balance(new_balance)
 
+# this function takes the current_amount, (the read_balance()), and subtracts the withdraw_amount from it to to create a new balance, then calls the write_balance(new_balance) function to display the new amount
 def make_withdraw(withdraw_amount):
     current_amount = read_balance()
     new_balance = int(current_amount) - int(withdraw_amount)
@@ -42,24 +46,36 @@ def write_savings(new_balance):
     f = open("savings.txt", "w")
     f.write(str(new_balance))
 
+# this function calls the menu() function to prompt the user on how they wish to proceed. 
 def main():
     menu()
     option = input(prompt)
     print("\n")
+
+# if the user picks opt 1., it will display their current balance by calling the read_balance() function, then calls the main() function to continue.
     if option == "1":
         print("Your current balance is "+ read_balance())  
         print("\n")
         input("Press 'ENTER' to continue.")
         main()  
 
+# if the user picks opt 2., it prompts user to enter withdraw amount, then calls the make_withdraw(withdraw_amount) function to perform the operation and prints the read_balance() to show new amount.
     elif option == "2":
         withdraw_amount = input("How much do you wish to withdraw?\n")
-        make_withdraw(withdraw_amount)
-        print("Your new balance is " + read_balance())
-        print("\n")
-        input("Press 'ENTER' to continue.")
+        
+########## in order to get a comparisson for the number in the 'if' statement, I need to convert the str to a float        
+        if int(read_balance()) > 0: 
+            make_withdraw(withdraw_amount)
+            print("Your new balance is " + read_balance())
+            print("\n")
+            input("Press 'ENTER' to continue.")
+        else: 
+            print("Insufficient funds. \n")
+        
+
         main()
 
+# if user picks opt 3., user is prompted to enter deposit amount then the make_deposit(deposit_amount) is called, then read_balance() function displays new balance after deposit
     elif option == "3":
         deposit_amount = input("How much to do you wish to deposit?\n")
         make_deposit(deposit_amount)
@@ -68,25 +84,32 @@ def main():
         input("Press 'ENTER' to continue.")
         main()
 
+###
     elif option == "4":
         print("Your current balance is " + read_savings() + " in your savings.")
         print("\n")
         input("Press 'ENTER' to continue.")
         main()
 
+# if user picks opt 4., prints exit string and exits the application
     elif option == "5":
         print("Thank you, have a great day!\n")
 
+# if user picks option not listed, prints sorry statement the calls main() function to bring back to main menu and pick again.
     else:
         print("Sorry, that is an invalid option.\n")
         main()
 
 main()
 
+
+
+
+
 ### things to do:
     # make amounts floats
     # transfer from checking to savings
-    # 
+    # negative balance
 
 # print("How would you like to proceed?")
 # print("""
